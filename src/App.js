e86a447c82806cc2161300fd3da28164a8a4cbd2
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-06-22 10:23:32 -0400

    fixed month switching

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/header';
+import Calendar from './components/calendar';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStroopwafel);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <Calendar />
       </div>
     );
   }
---
+++ src/components/calendar.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import DayNames from './dayNames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Week from './week';
+
+class Calendar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      month: moment(),
+      selected: moment().startOf('day')
+    };
+
+    this.prevMonth = this.prevMonth.bind(this);
+    this.nextMonth = this.nextMonth.bind(this);
+  }
+
+  prevMonth(e){
+    e.preventDefault();
+    const { month } = this.state;
+    const newMonth = month.subtract(2, 'month');
+    this.setState({
+      month: newMonth,
+    });
+  }
+
+  nextMonth(e){
+    e.preventDefault();
+    const { month } = this.state;
+    const newMonth = month.add(0.1, 'month');
+    this.setState({
+      month: newMonth,
+    });
+  }
+
+  daySelect(day){
+    this.setState({
+     selected: day.date,
+     month: day.date.clone(),
+   });
+  }
+
+  createWeeks(){
+    let weeks = [];
+    let done = false;
+    let date = this.state.month.startOf('month').add('w', -1).day('Sunday');
+    let count = 0;
+    let monthIndex = date.month();
+
+    const { selected, month } = this.state;
+
+    while (!done) {
+      weeks.push(
+        <Week key={date}
+          date={date.clone()}
+          month={month}
+          select={(day)=>this.daySelect(day)}
+          selected={selected} />
+      );
+
+      date.add(1, "w");
+
+      done = count++ > 2 && monthIndex !== date.month();
+      monthIndex = date.month();
+    }
+
+    return weeks;
+  }
+
+  renderMonthLabel() {
+    // debugger
+    const { month } = this.state;
+
+    return <span className="month-label">{month.format("MMMM YYYY")}</span>;
+  }
+
+  render(){
+    return (
+     <section className="calendar">
+       <header className="header">
+         <div className="month-display row">
+           <h1 onClick={this.prevMonth}>◁</h1>
+           {this.renderMonthLabel()}
+           <h1 onClick={this.nextMonth}>▷</h1>
+         </div>
+         <DayNames />
+       </header>
+       {this.createWeeks()}
+     </section>
+   );
+  }
+}
+
+export default Calendar;
